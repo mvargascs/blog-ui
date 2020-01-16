ea5241e60190cb27880de6866c30625cee2cbdcd
Miguel Vargas <mvargasuo@gmail.com>
2020-01-16 16:16:38 -0500

    Create Blog Component has config service injected to change toolbar title. Catch all sub implemented. Request subbed to and added to sub. createBlog function made to send over the request. Implemented on destroy to unsub from component sub.

--- src/app/pages/create-blog/create-blog.component.ts
+++ src/app/pages/create-blog/create-blog.component.ts
@@ -1,30 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Blog } from '@shared/models/Blog';
 import { HttpClient } from '@angular/common/http';
+import { ConfigService } from '@shared/services/config.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'migs-create-blog',
   templateUrl: './create-blog.component.html',
   styleUrls: ['./create-blog.component.scss']
 })
-export class CreateBlogComponent implements OnInit {
-
+export class CreateBlogComponent implements OnInit, OnDestroy {
   blogForm: FormGroup;
+  subs: Subscription = new Subscription();
 
   constructor(
+    private config: ConfigService,
     private fb: FormBuilder,
     private http: HttpClient,
-  ) {}
+  ) { }
 
   ngOnInit() {
+    this.config.changeTitle('Create Blog');
+
     this.blogForm = this.fb.group({
       title: [''],
       content: ['']
     });
   }
 
-  createBlog(blog: Blog) {
-    return this.http.post("http://localhost:8080/mock/blog", blog);
+  ngOnDestroy() {
+    this.subs.unsubscribe();
+  }
+
+  createBlog() {
+    const blog: Blog = {
+      title: this.blogForm.value.title,
+      content: this.blogForm.value.content
+    }
+
+    this.subs.add(
+      this.http.post('http://localhost:8080/mock/blog', blog).subscribe()
+    );
   }
 }
