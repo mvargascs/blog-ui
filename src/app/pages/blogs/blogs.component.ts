e93d1f20ac43dcceb02bca5db2bf7dc10b1d4f18
Miguel Vargas <mvargasuo@gmail.com>
2020-01-16 16:13:14 -0500

    Injected config service and change title on init.

--- src/app/pages/blogs/blogs.component.ts
+++ src/app/pages/blogs/blogs.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Blog } from '@shared/models/Blog';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { ConfigService } from '@shared/services/config.service';
 
 @Component({
   selector: 'migs-blogs',
@@ -12,10 +13,13 @@ export class BlogsComponent implements OnInit {
   blogs$: Observable<Blog[]>;
 
   constructor(
+    private config: ConfigService,
     private http: HttpClient
   ) {}
 
   ngOnInit() {
-    this.blogs$ = this.http.get<Blog[]>("http://localhost:8080/mock/blog");
+    this.config.changeTitle('Blogs');
+
+    this.blogs$ = this.http.get<Blog[]>('http://localhost:8080/mock/blog');
   }
 }
