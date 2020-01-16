dce555af2400c47def717b4cc36736c165973d0b
Miguel Vargas <mvargasuo@gmail.com>
2020-01-16 16:14:13 -0500

    Material, Flex, and Reactive modules added to create blog module.

--- src/app/pages/create-blog/create-blog.module.ts
+++ src/app/pages/create-blog/create-blog.module.ts
@@ -3,13 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { CreateBlogRoutingModule } from './create-blog-routing.module';
 import { CreateBlogComponent } from './create-blog.component';
-
+import { MaterialModule } from '@shared/material/material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [CreateBlogComponent],
   imports: [
     CommonModule,
-    CreateBlogRoutingModule
+    CreateBlogRoutingModule,
+    MaterialModule,
+    FlexLayoutModule,
+    ReactiveFormsModule,
   ]
 })
 export class CreateBlogModule { }
